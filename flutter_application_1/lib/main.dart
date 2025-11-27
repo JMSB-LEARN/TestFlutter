13503c972e2a9999afbc288a27cf74397c3e9a10
JMSBD <jmarquess04@iesalbarregas.es>
2025-11-27 22:52:11 +0100

    Finish

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -12,10 +12,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 36, 221, 7),
+        ),
         useMaterial3: true,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: const Color.fromARGB(255, 36, 221, 7),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      home: const MyHomePage(title: 'Flutter Demo Alterada'),
     );
   }
 }
@@ -31,10 +37,53 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter(int quantity) {
+    if (quantity < 0) {
+      return;
+    }
     setState(() {
-      _counter++;
+      _counter += quantity;
     });
+  }
+
+  void _decrementCounter(int quantity) {
+    if (quantity < 0) {
+      return;
+    }
+    setState(() {
+      _counter -= quantity;
+    });
+  }
+
+  //¿Deberia usar este metodo?
+  void _multiplyCounter(int quantity) {
+    if (_counter != 0) {
+      setState(() {
+        _counter *= quantity;
+      });
+    }
+  }
+
+  //¿O este metodo?
+  void _divideCounter(int quantity) {
+    if (quantity < 0) {
+      return;
+    }
+    if (_counter < 0) {
+      if ((_counter / quantity).ceil() == 0) {
+        return;
+      }
+      setState(() {
+        _counter = (_counter / quantity).ceil();
+      });
+    } else {
+      if ((_counter / quantity).floor() == 0) {
+        return;
+      }
+      setState(() {
+        _counter = (_counter / quantity).floor();
+      });
+    }
   }
 
   @override
@@ -48,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Number of Pineapples:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -56,10 +105,84 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () => _incrementCounter(1),
+            tooltip: 'Increment 1',
+            child: const Icon(Icons.exposure_plus_1),
+          ),
+          const SizedBox(width: 12),
+
+          FloatingActionButton(
+            onPressed: () => _incrementCounter(2),
+            tooltip: 'Increment 2',
+            child: const Icon(Icons.exposure_plus_2),
+          ),
+          const SizedBox(width: 24),
+
+          FloatingActionButton(
+            onPressed: () => _decrementCounter(1),
+            tooltip: 'Decrement 1',
+            child: const Icon(Icons.exposure_minus_1),
+          ),
+          const SizedBox(width: 12),
+
+          FloatingActionButton(
+            onPressed: () => _decrementCounter(2),
+            tooltip: 'Decrement 2',
+            child: const Icon(Icons.exposure_minus_2),
+          ),
+          const SizedBox(width: 24),
+
+          FloatingActionButton(
+            onPressed: () => _multiplyCounter(2),
+            tooltip: 'Multiply 2',
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              children: <Widget>[
+                Icon(Icons.close, semanticLabel: 'Multiply icon'),
+                Icon(Icons.looks_two),
+              ],
+            ),
+          ),
+          const SizedBox(width: 12),
+
+          FloatingActionButton(
+            onPressed: () => _multiplyCounter(3),
+            tooltip: 'Multiply 3',
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(Icons.close, semanticLabel: 'Multiply icon'),
+                Icon(Icons.looks_3),
+              ],
+            ),
+          ),
+          const SizedBox(width: 24),
+
+          FloatingActionButton.extended(
+            onPressed: () => _divideCounter(2),
+            label: const Text("÷ 2"),
+          ),
+          const SizedBox(width: 12),
+
+          FloatingActionButton.extended(
+            onPressed: () => _divideCounter(10),
+            label: const Text("÷ 10"),
+          ),
+          const SizedBox(width: 24),
+
+          FloatingActionButton.extended(
+            onPressed: () => _multiplyCounter(-1),
+            label: const Text("∓"),
+          ),
+          const SizedBox(width: 24),
+        ],
       ),
     );
   }
